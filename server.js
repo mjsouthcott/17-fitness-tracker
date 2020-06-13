14f06bf9d28181bbee330fae349f4c0697950d1a
Matthew Southcott <mjsouthcott@gmail.com>
2020-06-13 14:31:44 -0400

    Add model files

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 app.use(express.static('public'))
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
@@ -18,7 +18,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
 })
 
 // routes
-app.use(require('./routes/api.js'))
+app.use(require('./routes/api-routes.js'))
+app.use(require('./routes/html-routes.js'))
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`)
